21349293a9f84f3ad1466a95bd61d5be1868e79c
Damo <ddamrod@gmail.com>
2021-01-19 01:29:30 -0300

    commit

--- commands/play.js
+++ commands/play.js
@@ -133,7 +133,7 @@ module.exports.run = async(client, message, args, queue, searcher, spotifyApi) =
     function autoplay(videoId){
         var str = videoId;
         google.youtube('v3').search.list({
-            key: process.env.youtube_ap,
+            key: 'AIzaSyDqvVgBxU3_aobQxxU49F7jUwLp-TCnhjY',
             part: 'snippet',
             type: 'video', 
             videoDuration: 'medium',
